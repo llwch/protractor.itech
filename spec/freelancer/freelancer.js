e149f260227ea47e8d4687e939d64ac9ed6db854
Anton Menshutkin <anton.mianshutkin@itechart-group.com>
2018-07-31 12:56:18 +0300

    Add async

--- spec/freelancer/freelancer.js
+++ spec/freelancer/freelancer.js
@@ -1,30 +1,34 @@
 describe('login to the freelancer.com', function() {
-    let loginLink = element(by.className('LoginSection-btn'));
+    let loginLink = element(by.css("[data-uitest-target='login-button']"));
   
-    beforeEach(function() {
-        browser.get('https://www.freelancer.com/');
+    beforeEach(async function() {
+        await browser.get('https://www.freelancer.com/');
     });
   
-    it('should verify title name', function() {
+    it('should verify title name', async function() {
         expect(browser.getTitle()).toEqual('Hire Freelancers & Find Freelance Jobs Online - Freelancer');
     });
   
-    it('should open login modal', function(){
-        loginLink.click();
-        browser.sleep(10000);
-        expect(browser.driver.getCurrentUrl()).toEqual("https://www.freelancer.com/login");
+    it('should open login modal', async function(){
+        await loginLink.click();
+        await browser.refresh();
+        expect(browser.getCurrentUrl()).toEqual("https://www.freelancer.com/login");
     });
 
     it('should login to app', async function() {
-        let loginField = element(by.name('Email or Username'));
-        let passwordField = element(by.name('Password'));
+        //await browser.get('https://www.freelancer.com/login');
+        await loginLink.click();
+        await browser.refresh();
+
+        let loginField = element(by.id('username'));
+        let passwordField = element(by.id('password'));
         let loginButton = element(by.id('login_btn'));
         
         await loginField.sendKeys('abyss.itech@gmail.com');
         await passwordField.sendKeys('12qwaszx');
-    
-        loginButton.click();
-        browser.sleep(3000);
-      expect(browser.driver.getCurrentUrl()).toEqual("https://www.freelancer.com/dashboard/");
-    });  
+            
+        await loginButton.click();
+        await browser.sleep(10000);
+        expect(browser.getCurrentUrl()).toEqual("https://www.freelancer.com/dashboard/");
+    });
   });
