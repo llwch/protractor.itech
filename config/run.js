77879c203dc5d81d223ea482e3f62d54b9a819e6
Anton Menshutkin <anton.mianshutkin@itechart-group.com>
2018-08-05 21:49:56 +0300

    change path

--- config/run.js
+++ config/run.js
@@ -3,7 +3,7 @@ var env = require('./environment.js');
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: env.seleniumAddress,
-  specs: ['../spec/freelancer/freelancer.js'],
+  specs: ['../step_definitions/login/freelancer.js'],
   capabilities: env.capabilities,
     jasmineNodeOpts: {
       showColors: true,
